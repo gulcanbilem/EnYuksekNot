80a46efe295825b0726c891b27916ee2e42e37a6
gulcanbilem <113993110+gulcanbilem@users.noreply.github.com>
2023-01-01 20:06:12 +0300

    Add files via upload

---
+++ EnYuksekNotAlanlar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnYuksekNotAlanlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c, EnBuyuk = 0, yer = 0;
+            String[] A = { "Emin", "Emir", "İclal", "Bekir", "Cem", "Arda", "Kaan", "Metin", "Can", "Ramiz" };
+            int[] B = { 56, 78, 95, 23, 95, 87, 95, 77, 45, 33 };
+            for (c = 0; c <= B.Length - 1; c++)
+            {
+                for (c = 0; c <= B.Length - 1; c++)
+                {
+                    if (B[c] >= EnBuyuk)
+                    {
+                        EnBuyuk = B[c];
+                        yer = c;
+                    }
+                }
+                if (yer != 0)
+                {
+                    Console.WriteLine("En Yüksek Notu Alan Kişi: " + A[(yer)]);
+                    B[yer] = 0;
+                    yer = 0;
+                    c = 0;
+                }
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
